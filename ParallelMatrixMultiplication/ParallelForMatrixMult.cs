70e48f977944e91a9783b45b8796796f81b34922
calebherne <calebherne@gmail.com>
2018-10-22 19:19:00 -0400

    added files manually

---
+++ ParallelMatrixMultiplication/ParallelForMatrixMult.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelMatrixMultiplication
+{
+    public class ParallelForMatrixMult
+    {
+
+        public void MultiplyMatricesSequential(double[,] matA, double[,] matB,
+                                                double[,] result)
+        {
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
+
+            for (int i = 0; i < matARows; i++)
+            {
+                for (int j = 0; j < matBCols; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] += temp;
+                }
+            }
+        }
+
+        public void MultiplyMatricesParallel(double[,] matA, double[,] matB, double[,] result)
+        {
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
+
+            // A basic matrix multiplication.
+            // Parallelize the outer loop to partition the source array by rows.
+            Parallel.For(0, matARows, i =>
+            {
+                for (int j = 0; j < matBCols; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }); // Parallel.For
+        }
+
+        public List<long> RunMatrixMultWithTime(double[,] m1, double[,] m2)
+        {
+
+            int rowCount = m1.GetLength(0);
+            int colCount2 = m2.GetLength(1);
+ 
+            double[,] result = new double[rowCount, colCount2];
+
+            // sequential version.
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            MultiplyMatricesSequential(m1, m2, result);
+            stopwatch.Stop();
+            List<long> times = new List<long>();
+            times.Add(stopwatch.ElapsedMilliseconds);
+
+            // Reset timer and results matrix. 
+            result = new double[rowCount, colCount2];
+
+            // parallel loop.
+            stopwatch = Stopwatch.StartNew();
+            MultiplyMatricesParallel(m1, m2, result);
+            stopwatch.Stop();
+            times.Add(stopwatch.ElapsedMilliseconds);
+
+            // Keep the console window open in debug mode.
+            Console.Error.WriteLine("Press any key to exit.");
+            return times;
+        }
+    }
+}
---
+++ ParallelMatrixMultiplication/TaskMatrixMult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelMatrixMultiplication
+{
+    public class TaskMatrixMult
+    {
+        public double[,] MatA { get; set; }
+        public double[,] MatB { get; set; }
+        public double[,] Result { get; set; }
+
+        public TaskMatrixMult(double[,] m1, double[,] m2)
+        {
+            MatA = m1;
+            MatB = m2;
+            Result = new double[MatA.GetLength(0), MatB.GetLength(1)];
+        }
+
+        public long RunTaskMatrix()
+        {
+            Task[] taskList = new Task[MatA.GetLength(0)];
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for(int i = 0; i < MatA.GetLength(0); i++)
+            {
+                var temp = i;
+                var task = new Task(() => {
+                    MultiplyOneRow(temp);
+                });
+                task.Start();
+                taskList[i] = task;
+            }
+            Task.WaitAll(taskList);
+            stopwatch.Stop();
+            
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private void MultiplyOneRow(int row)
+        {
+            for (int j = 0; j < MatB.GetLength(1); j++)
+            {
+                double temp = 0;
+                for (int k = 0; k < MatA.GetLength(1); k++)
+                {
+                    temp += MatA[row, k] * MatB[k, j];
+                }
+                Result[row, j] = temp;
+            }
+
+        }
+    }
+}
